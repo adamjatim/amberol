cb27aa69b3bad474dc38592a118a3d24ce7b2987
adamjatim <adamrahmatilahi@gmail.com>
2022-11-28 12:04:14 +0700

    audio: Bridge repeat mode to MPRIS

---
+++ src/audio/controller.rs
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: 2022  Emmanuele Bassi
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+use crate::audio::{PlaybackState, RepeatMode, Song};
+
+pub trait Controller {
+    fn set_playback_state(&self, state: &PlaybackState);
+
+    fn set_song(&self, song: &Song);
+    fn set_position(&self, position: u64);
+    fn set_repeat_mode(&self, repeat: RepeatMode);
+}
